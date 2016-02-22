78f558d85fb208923c872c8f54592361a511ab5e
billxinwu <Bill.Wu@experianinteractive.com>
2016-02-22 14:34:16 -0800

    added from local
    added from local

---
+++ TestFolder1/ReadAllSQLErrorLogs.sql
@@ -0,0 +1,20 @@
+set nocount on
+
+declare @i int = 0
+while @i < 10
+begin
+
+	exec sp_readerrorlog @i, 1, 'vertex', 'Login failed for user'
+
+	select @i = @i + 1
+
+end
+
+
+Go
+
+sp_who2 active
+
+
+
+
